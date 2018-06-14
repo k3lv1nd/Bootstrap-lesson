3ef71a6ebce9fb558800fb92efa6034461a4e4a8
k3lv1nd <kalvind95@gmail.com>
2018-06-14 23:47:18 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<title>bootstrap</title>
+
+</head>
+<body id="mybody" >
+<div class="container">
+
+<div class="row">
+	<div class="row"> 
+	<h1 >Welcome to our Eat well restaurant</h1>
+	<form class="form-gro">
+      <input class="form-control " type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+    </div>
+</div>
+
+
+<div class="row  ">
+<div><p>Please register with us to enjoy our delicacies</p><div>
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<div class=col-3>
+</div>
+<div class=col-4>
+</div>
+
+
+	</div>
+
+</div>
+
+	
+</div>
+
+</body>
+</html>
