6e114e32b769aea5cf027a77a9f782754063b27b
k3lv1nd <kalvind95@gmail.com>
2018-06-18 08:30:41 +0300

    Second commit

--- index.php
+++ index.php
@@ -9,20 +9,19 @@
 <div class="container">
 
 <div class="row">
-	<div class="row"> 
 	<h1 >Welcome to our Eat well restaurant</h1>
   <div class="float-left">
-  <form class="form-group float-left">
+  <form style="padding-left:190px;" class="form-group float-left">
       <input class="form-control-sm" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success" type="submit">Search</button>
     </form>
-    </div>
+   
   </div>
 </div>
 
 
 <div class="row" style="margin-top: 30px">
-<div class="col-4"><p id="intro">Register with us to become a member and  enjoy our  wide range of delicacies</p><div>
+<div class="col-sm-4"><p id="intro">Register with us to become a member and  enjoy our  wide range of delicacies</p>
 <form>
   <div class="form-group">
     <label for="exampleInputEmail1">Email</label>
@@ -47,15 +46,11 @@
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </div>
-<div>
-<div class="row">
-<div>
-    <a href="#" class="thumbnail" style="height: 350px; width: 300px; margin-top:100px">
+<div class="col-sm-4 offset-2">
+
+    <a href="#" class="thumbnail" style="height: 350px; width: 300px; margin-top:200px; margin-left:350px;">
       <img class="glass" src="restuarant.jpg" style="height:300px; width:300px;">
     </a>
-    <div class="col-4">
-    </div>
-    </div>
  
 
 
@@ -63,7 +58,14 @@
 	</div>
 
 </div>
-
+<div class="row">
+   <div class=" col-md-12 jumbotron" style="margin-top: 50px">
+  <p class="welcome" style="color: #0d8930">Eatwell restaurant, located in the heart of the Fox Valley is a 100 seat family-dining restaurant that is open seven days a week. It has successfully been operating since March 12, 1998, at 2220 East Northland Avenue, Appleton, WI. The focus on our menu is American / Mexican / Greek / Italian offerings. 
+The six-page menu has offers omelettes, skillets, melts, sandwiches, gyros, burgers, dinner offerings, stir frys, pasta offerings, Mexican offerings, salads, homemade soups made in our kitchen, different dessert offerings, beer, wine and mixed drinks, coffees, teas, and soft drinks. Additionally, there are weekly featured items that change with seasons of the year. Friday specials, and a “Lite” menu that features items that tantalize customers throughout the year in accordance with the four seasons influencing Wisconsinites taste buds. Prices are competitive with other family restaurants in the area.</p>
+  <p>...</p>
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+</div>
+</div>
 	
 </div>
 
