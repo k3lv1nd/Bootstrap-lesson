eb492fa76aad00b1f64774cdc65305bb20f414ee
k3lv1nd <kalvind95@gmail.com>
2018-06-18 06:58:45 +0300

    Initial commit

--- index.php
+++ index.php
@@ -5,41 +5,59 @@
 	<title>bootstrap</title>
 
 </head>
-<body id="mybody" >
+<body class="mybody">
 <div class="container">
 
 <div class="row">
 	<div class="row"> 
 	<h1 >Welcome to our Eat well restaurant</h1>
-	<form class="form-gro">
-      <input class="form-control " type="search" placeholder="Search" aria-label="Search">
+  <div class="float-left">
+  <form class="form-group float-left">
+      <input class="form-control-sm" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success" type="submit">Search</button>
     </form>
     </div>
+  </div>
 </div>
 
 
-<div class="row  ">
-<div><p>Please register with us to enjoy our delicacies</p><div>
+<div class="row" style="margin-top: 30px">
+<div class="col-4"><p id="intro">Register with us to become a member and  enjoy our  wide range of delicacies</p><div>
 <form>
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <label for="exampleInputEmail1">Email</label>
+    <input type="text" class="form-control" style="margin-top: 5px;" placeholder="Enter Email" >
   </div>
   <div class="form-group">
+    <label for="exampleInputPassword1">username</label>
+    <input type="text" class="form-control" style="margin-top: 5px;" placeholder="text">
+  </div>
+   <div class="form-group">
+    <label for="exampleInputPassword1">Phone number</label>
+    <input type="password" class="form-control" style="margin-top: 5px;" placeholder="phone Number">
+  </div>
+   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input type="password" class="form-control" style="margin-top: 5px;" placeholder="Password">
   </div>
   <div class="form-group form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <label class="form-check-label" for="exampleCheck1">Save my password</label>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-<div class=col-3>
 </div>
-<div class=col-4>
-</div>
+<div>
+<div class="row">
+<div>
+    <a href="#" class="thumbnail" style="height: 350px; width: 300px; margin-top:100px">
+      <img class="glass" src="restuarant.jpg" style="height:300px; width:300px;">
+    </a>
+    <div class="col-4">
+    </div>
+    </div>
+ 
+
 
 
 	</div>
